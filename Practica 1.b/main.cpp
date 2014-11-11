c6e84a52524f5e695f694ed0b005fabe2be5ecc9
Bretto9 <jose.bretto@gmail.com>
2014-11-11 17:18:10 +0100

    Estado inicial
    Estado inicial del repositorio

---
+++ Practica 1.b/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: JoseDavid
+ *
+ * Created on 11 de noviembre de 2014, 17:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
